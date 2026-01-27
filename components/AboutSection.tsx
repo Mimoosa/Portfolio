44777d2b1a1f977032283c8d5c5555bcd75c2df4
Monami <monami.saeki@gmail.com>
2026-01-27 14:35:19 +0200

    update cv

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -51,7 +51,7 @@ const AboutSection = () => {
             </p>
             <br />{" "}
             <a
-              href="/cv/Monami_CV.pdf"
+              href="/cv/Monami_Kirjavainen_CV_2026.pdf"
               target="_blank"
               rel="noopener noreferrer"
               className="inline-block mt-4 px-4 py-2 bg-teal-600 text-white font-semibold rounded hover:bg-teal-700 transition"
